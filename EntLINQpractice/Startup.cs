c3558fd9031ac3ec0cf8c3222294d1777afb7992
Ray Meghani <rehman.a.meghani@gmail.com>
2014-04-08 10:54:04 -0500

    Initial Commit

---
+++ EntLINQpractice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EntLINQpractice.Startup))]
+namespace EntLINQpractice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
